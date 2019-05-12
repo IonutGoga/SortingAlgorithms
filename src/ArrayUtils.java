5c773ea802534c95702ed4eb7e4ada6e893bd5ac
IonutG <ionut_l3@yahoo.com>
2019-05-12 13:49:00 -0800

    seven

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,8 +1,8 @@
 
 public class ArrayUtils {
 
-    public static void show (int [] a){
-        for(int i = 0; i < a.length; i ++){
+    public static void show(int[] a) {
+        for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
         System.out.println();
--- src/MyInsertionSort.java
+++ src/MyInsertionSort.java
@@ -1,12 +1,12 @@
 public class MyInsertionSort {
 
-    public static void sort(int[] a){
+    public static void sort(int[] a) {
 
-        for(int i = 1; i < a.length; i++){
+        for (int i = 1; i < a.length; i++) {
             int aux = a[i];
             int j = i;
 
-            while(j > 0 && a[j - 1] > aux){
+            while (j > 0 && a[j - 1] > aux) {
                 a[j] = a[j - 1];
                 j--;
             }
